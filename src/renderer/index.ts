7767709b36fcd2bb8061c00ce973626a57964218
Steve Kinney <hello@stevekinney.net>
2023-08-05 17:44:30 -0600

    Enable and disable the save button

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -9,17 +9,23 @@ import {
 } from './elements';
 
 let currentFilePath: string;
+let lastSavedMarkdown: string;
 
 const renderMarkdown = async (content: string) => {
   const html = await toHTML(content);
   Rendered.innerHTML = html;
 };
 
+const hasUnsavedChanges = (markdown: string): boolean => {
+  if (!markdown) return false;
+  return lastSavedMarkdown !== markdown;
+};
+
 Markdown.addEventListener('input', async () => {
   const markdown = Markdown.value;
   const html = await toHTML(markdown);
 
-  SaveMarkdown.disabled = !markdown;
+  SaveMarkdown.disabled = !hasUnsavedChanges(markdown);
 
   Rendered.innerHTML = html;
 });
@@ -28,7 +34,8 @@ OpenFile.addEventListener('click', async () => {
   const file = await window.file.open();
   if (!file) return;
 
-  currentFilePath = file.path;
+  updateCurrentFile(file.path, file.content);
+  lastSavedMarkdown = file.content;
 
   Markdown.value = file.content;
   renderMarkdown(file.content);
@@ -38,7 +45,13 @@ SaveMarkdown.addEventListener('click', async () => {
   const content = Markdown.value;
   if (!content) return;
 
-  await window.file.saveMarkdown(content, currentFilePath || '');
+  const { path } = await window.file.saveMarkdown(
+    content,
+    currentFilePath || '',
+  );
+
+  updateCurrentFile(path, content);
+  lastSavedMarkdown = content;
 });
 
 SaveHtml.addEventListener('click', async () => {
